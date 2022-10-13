d06ad30a801a09834b5300f507b66b7fbde4cf3d
Oskar Niesen <oskar-github@niesens.com>
2022-10-13 18:07:35 -0500

    Changed benchmark tests

--- src/SortBenchmarks/SorterBenchmarks.java
+++ src/SortBenchmarks/SorterBenchmarks.java
@@ -8,10 +8,10 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Fork(value = 2)
 @Warmup(iterations = 2)
-@Measurement(iterations = 100, timeUnit = TimeUnit.MILLISECONDS, time = 1)
+@Measurement(iterations = 100, timeUnit = TimeUnit.NANOSECONDS, time = 1)
 public class SorterBenchmarks {
     @Benchmark
     public void mergeSortThreadsBenchmark() {
@@ -20,12 +20,12 @@ public class SorterBenchmarks {
 
     @Benchmark
     public void mergeSortBenchmark() {
-        MergeSortThreads.sortArray(getRandomArray().clone());
+        MergeSort.sortArray(getRandomArray().clone());
     }
 
     Comparable[] getRandomArray() {
-        Random random = new Random();
-        Integer[] integers = new Integer[random.nextInt(10,30)];
+        Random random = new Random(2);
+        Integer[] integers = new Integer[500000];
         for (int i = 0; i < integers.length; i++) {
             integers[i] = random.nextInt();
         }
